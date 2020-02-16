c0ec99c8a42e5c8377b579051ceeb050af9195ff
Antti Rauhala / pilvikoodari.net <anttirauhala@users.noreply.github.com>
2020-02-16 22:30:03 +0200

    Add API function to fetch data

---
+++ api/api.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import {IceHole} from 'models/iceHole';
+
+const apiKey = ''; // TODO: get API key from .env file
+const iceHolesUrl =
+  'https://51zfnyhbyh.execute-api.us-east-1.amazonaws.com/default/avannot';
+
+const getIceHoles = (): Promise<IceHole[]> => {
+  return axios
+    .get(iceHolesUrl, {
+      headers: {
+        'x-api-key': apiKey,
+      },
+    })
+    .then(res => res.data.iceHoles);
+};
+
+export {getIceHoles};
