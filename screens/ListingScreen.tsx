6ae764893a488827436c3b538956c12d75ec547b
Antti Rauhala / pilvikoodari.net <anttirauhala@users.noreply.github.com>
2020-02-16 22:46:54 +0200

    Fetch and show some data in listing screen

--- screens/ListingScreen.tsx
+++ screens/ListingScreen.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-import {styles} from '../styles';
+import React, {useState, useEffect} from 'react';
+import {FlatList, SafeAreaView, Text, View} from 'react-native';
 
-const ListingScreen = () => (
-  <View style={styles.tabs}>
-    <Text style={styles.headerText}>Listing...</Text>
-  </View>
-);
+import {getIceHoles} from '../api/api';
+import {styles} from '../styles';
+import {IceHole} from 'models/iceHole';
+
+const ListingScreen = () => {
+  const [iceHoles, setIceHoles] = useState<IceHole[] | null>([]);
+  const [loading, setLoading] = useState<boolean | null>(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const data: IceHole[] = await getIceHoles();
+      setIceHoles(data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.tabs}>
+      {loading ? (
+        <Text style={styles.bodyText}>Ladataan tietoja...</Text>
+      ) : (
+        <FlatList
+          data={iceHoles}
+          renderItem={({item}) => <Text>{item.name}</Text>}
+          keyExtractor={item => item.id}
+          ListEmptyComponent={() => (
+            <Text style={styles.bodyText}>Ei saunoja :(</Text>
+          )}
+        />
+      )}
+    </SafeAreaView>
+  );
+};
 
 export default ListingScreen;
